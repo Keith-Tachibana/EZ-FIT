55502f9b0663bd29c59a401815a6ab10f2b745ca
ashwinbalachandran <ash20win@gmail.com>
2019-10-13 11:43:11 -0700

    Re-adding removed functionality for Onboarding

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SignInSide from './components/SignInSide'
-ReactDOM.render(<SignInSide />, document.getElementById('root'))
+import { BrowserRouter } from 'react-router-dom'
+import Main from './components/Main'
+import * as serviceWorker from './serviceWorker'
+
+ReactDOM.render((
+    <BrowserRouter>
+        <Main />
+    </BrowserRouter>
+    ), document.getElementById('root'));
+serviceWorker.unregister()
