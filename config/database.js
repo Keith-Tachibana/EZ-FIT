f33afa89f5a12b33408dcc355db1b3f79560caa9
ashwinbalachandran <ash20win@gmail.com>
2019-11-04 10:24:11 -0800

    Added MongoDB URI for mLab database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost:27017/ezphr'
+const mongoDB = 'mongodb://heroku_vc9ppq5b:o34ra1rle59heerll10fbco66d@ds141228.mlab.com:41228/heroku_vc9ppq5b'
 mongoose.connect(mongoDB, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 module.exports = mongoose;
