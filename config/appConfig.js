fc7d3fe92d7fc07bba979f8844c542cd349d7719
ashwinbalachandran <ash20win@gmail.com>
2019-11-20 18:29:17 -0800

    Fixing package-json

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,17 +1,15 @@
 const port = process.env.PORT || 8080;
 const saltRounds = process.env.SALT_ROUNDS || 12;
-const secretKey =
-	process.env.SECRET_KEY || 'TEST_KEY';
+const secretKey = process.env.SECRET_KEY || 'TEST_KEY';
 const clientID = '22BC4H';
-const clientSecret =
-	'705322f417660fbfd1ffe1bc86ef80e5';
-const mailgunApiKey = process.env.MAILGUN_API_KEY;
+const clientSecret = process.env.CLIENT_SECRET;
+const mailgunApiKey = process.env.MAILGUN_API_KEY || 'fakekey';
 
 module.exports = {
-	port,
-	saltRounds,
-	secretKey,
-	clientID,
-	clientSecret,
-	mailgunApiKey,
+    port,
+    saltRounds,
+    secretKey,
+    clientID,
+    clientSecret,
+    mailgunApiKey,
 };
