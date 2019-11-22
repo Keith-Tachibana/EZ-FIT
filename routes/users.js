8bf90f5b152a953049b9f305c68b44d513157cfb
ashwinbalachandran <ash20win@gmail.com>
2019-11-21 20:49:57 -0800

    Fixed routes for the new APIs

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,20 @@ const userController = require('../app/api/controller/users');
 const authController = require('../app/api/controller/authflow');
 const fitbitApiController = require('../app/api/controller/fitbitApiIntegration');
 
+/* Dashboard and User info routes */
 router.get('/getname', dashboardController.getNameById);
 router.get('/getpersonalinfo', dashboardController.getPersonalInfo);
 router.get('/signout', userController.signout);
 router.post('/updatepersonalinfo', dashboardController.updatePersonalInfo);
 router.post('/updatepassword', userController.updatePassword);
+
+/* Fitbit authorization with OAuth 2.0 routes*/
 router.get('/checkOAuthTokenStatus', authController.checkOAuthTokenStatus);
 router.post('/obtainToken', authController.obtainToken);
 router.post('/revokeToken', authController.revokeToken);
-router.get('/getcalories', dashboardController.getCalories);
-router.get('/getactivitysummary', dashboardController.getActivitySummary);
 
-router.get('/getUserSummary', fitbitApiController.getUserSummary);
+/* Fitbit API integration routes */
+router.get('/getcalories', fitbitApiController.getCalories);
+router.get('/getActivitySummary', fitbitApiController.getActivitySummary);
 
 module.exports = router;
