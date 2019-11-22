269604ab45bec5f6f1937609a3f5a8a591ec4b85
ashwinbalachandran <ash20win@gmail.com>
2019-11-21 21:37:46 -0800

    Fixed config formatting

--- config/appConfig.js
+++ config/appConfig.js
@@ -5,7 +5,8 @@ const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/ezfit';
 const saltRounds = process.env.SALT_ROUNDS || 12;
 const secretKey = process.env.SECRET_KEY || 'TEST_KEY';
 const clientID = process.env.FITBIT_CLIENT_ID || '22BC4H';
-const clientSecret = process.env.FITBIT_CLIENT_SECRET || "ENTER_CLIENT_KEY_HERE";
+const clientSecret =
+    process.env.FITBIT_CLIENT_SECRET || 'ENTER_CLIENT_KEY_HERE';
 const mailDomain = 'mail.ezfit.rocks';
 const mailgunApiKey = process.env.MAILGUN_API_KEY || 'ENTER_MAILGUN_KEY_HERE';
 
