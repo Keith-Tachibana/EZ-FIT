391539f3c75e4389b13739f737c43401eda6eba2
Harry <harrynp12@gmail.com>
2019-11-22 00:54:32 -0800

    Added initial weight goal component

--- client/src/components/DashboardContent.js
+++ client/src/components/DashboardContent.js
@@ -15,6 +15,7 @@ import CaloriesGoal from './CaloriesGoal';
 import HeartRateZones from './HeartRateZones';
 import RestingHeartRate from './RestingHeartRate';
 import Weight from './Weight';
+import WeightGoal from './WeightGoal';
 import ConnectDialog from './ConnectDialog';
 
 const useStyles = makeStyles(theme => ({
@@ -214,6 +215,10 @@ export default function DashboardContent() {
       getHeartRateData();
       getWeightData();
     }
+
+    useEffect(() => {
+      console.log(weight);
+    }, [weight])
     
     useEffect(() => {
       async function initialLoad() {
@@ -221,283 +226,6 @@ export default function DashboardContent() {
         getAllData();
       }
       initialLoad();
-
-      // setTimeout(function(){
-      //   setSteps({
-      //     current: 5121,
-      //     goal: 25000,
-      //   });
-      // }, 2000);
-      // setTimeout(function(){
-      //   setDistance({
-      //     current: 5.5,
-      //     goal: 10,
-      //   });
-      // }, 2000);
-      // setTimeout(function(){
-      //   setFloors({
-      //     current: 7,
-      //     goal: 10,
-      //   });
-      // }, 2000);
-      // setTimeout(function(){
-      //   setActiveMinutes({
-      //     current: 21,
-      //     goal: 30,
-      //   });
-      // }, 2000);
-      // setTimeout(function(){
-      //   setCalories({
-      //     current: 1845,
-      //     goal: 2950,
-      //   });
-      // }, 2000);
-
-      // setTimeout(function(){
-      //   setCaloriesBurnedData([
-      //     {"dateTime":"2011-04-27","value":5490},
-      //     {"dateTime":"2011-04-28","value":2344},
-      //     {"dateTime":"2011-04-29","value":2779},
-      //     {"dateTime":"2011-04-30","value":9196},
-      //     {"dateTime":"2011-05-01","value":15828},
-      //     {"dateTime":"2011-05-02","value":1945},
-      //     {"dateTime":"2011-05-03","value":366},
-      //     {"dateTime":"2011-05-04","value":10102},
-      //     {"dateTime":"2011-05-05","value":803},
-      //     {"dateTime":"2011-05-06","value":450},
-      //     {"dateTime":"2011-05-07","value":2010},
-      //     {"dateTime":"2011-05-08","value":1600},
-      //     {"dateTime":"2011-05-09","value":5050},
-      //     {"dateTime":"2011-05-10","value":300},
-      //     {"dateTime":"2011-05-11","value":11020},
-      //     {"dateTime":"2011-05-12","value":1800},
-      //     {"dateTime":"2011-05-13","value":1472},
-      //     {"dateTime":"2011-05-14","value":3414},
-      //     {"dateTime":"2011-05-15","value":9752},
-      //     {"dateTime":"2011-05-16","value":4542},
-      //     {"dateTime":"2011-05-17","value":121},
-      //     {"dateTime":"2011-05-18","value":12452},
-      //     {"dateTime":"2011-05-19","value":10101},
-      //     {"dateTime":"2011-05-20","value":2457},
-      //     {"dateTime":"2011-05-21","value":242},
-      //     {"dateTime":"2011-05-22","value":3671},
-      //     {"dateTime":"2011-05-23","value":214},
-      //     {"dateTime":"2011-05-24","value":707},
-      //     {"dateTime":"2011-05-25","value":247},
-      //     {"dateTime":"2011-05-26","value":2101},
-      //     {"dateTime":"2011-05-27","value":14080},
-      //   ]);
-      // }, 5000);
-
-      // setTimeout(function(){
-      //   setHeartRateData([
-      //     {
-      //         "dateTime": "2015-08-04",
-      //         "value": {
-      //             "customHeartRateZones": [],
-      //             "heartRateZones": [
-      //                 {
-      //                     "caloriesOut": 740.15264,
-      //                     "max": 94,
-      //                     "min": 30,
-      //                     "minutes": 593,
-      //                     "name": "Out of Range"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 249.66204,
-      //                     "max": 132,
-      //                     "min": 94,
-      //                     "minutes": 46,
-      //                     "name": "Fat Burn"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 160,
-      //                     "min": 132,
-      //                     "minutes": 0,
-      //                     "name": "Cardio"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 220,
-      //                     "min": 160,
-      //                     "minutes": 0,
-      //                     "name": "Peak"
-      //                 }
-      //             ],
-      //             "restingHeartRate": 68
-      //         }
-      //     },
-      //     {
-      //         "dateTime": "2015-08-05",
-      //         "value": {
-      //             "customHeartRateZones": [],
-      //             "heartRateZones": [
-      //                 {
-      //                     "caloriesOut": 740.15264,
-      //                     "max": 94,
-      //                     "min": 30,
-      //                     "minutes": 150,
-      //                     "name": "Out of Range"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 249.66204,
-      //                     "max": 132,
-      //                     "min": 94,
-      //                     "minutes": 20,
-      //                     "name": "Fat Burn"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 160,
-      //                     "min": 132,
-      //                     "minutes": 0,
-      //                     "name": "Cardio"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 220,
-      //                     "min": 160,
-      //                     "minutes": 30,
-      //                     "name": "Peak"
-      //                 }
-      //             ],
-      //             "restingHeartRate": 70
-      //         }
-      //     },
-      //     {
-      //         "dateTime": "2015-08-06",
-      //         "value": {
-      //             "customHeartRateZones": [],
-      //             "heartRateZones": [
-      //                 {
-      //                     "caloriesOut": 740.15264,
-      //                     "max": 94,
-      //                     "min": 30,
-      //                     "minutes": 30,
-      //                     "name": "Out of Range"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 249.66204,
-      //                     "max": 132,
-      //                     "min": 94,
-      //                     "minutes": 200,
-      //                     "name": "Fat Burn"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 160,
-      //                     "min": 132,
-      //                     "minutes": 250,
-      //                     "name": "Cardio"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 220,
-      //                     "min": 160,
-      //                     "minutes": 100,
-      //                     "name": "Peak"
-      //                 }
-      //             ],
-      //             "restingHeartRate": 51
-      //         }
-      //     },
-      //     {
-      //         "dateTime": "2015-08-07",
-      //         "value": {
-      //             "customHeartRateZones": [],
-      //             "heartRateZones": [
-      //                 {
-      //                     "caloriesOut": 740.15264,
-      //                     "max": 94,
-      //                     "min": 30,
-      //                     "minutes": 700,
-      //                     "name": "Out of Range"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 249.66204,
-      //                     "max": 132,
-      //                     "min": 94,
-      //                     "minutes": 20,
-      //                     "name": "Fat Burn"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 160,
-      //                     "min": 132,
-      //                     "minutes": 10,
-      //                     "name": "Cardio"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 220,
-      //                     "min": 160,
-      //                     "minutes": 25,
-      //                     "name": "Peak"
-      //                 }
-      //             ],
-      //             "restingHeartRate": 80
-      //         }
-      //     },
-      //     {
-      //         "dateTime": "2015-08-08",
-      //         "value": {
-      //             "customHeartRateZones": [],
-      //             "heartRateZones": [
-      //                 {
-      //                     "caloriesOut": 740.15264,
-      //                     "max": 94,
-      //                     "min": 30,
-      //                     "minutes": 0,
-      //                     "name": "Out of Range"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 249.66204,
-      //                     "max": 132,
-      //                     "min": 94,
-      //                     "minutes": 11,
-      //                     "name": "Fat Burn"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 160,
-      //                     "min": 132,
-      //                     "minutes": 15,
-      //                     "name": "Cardio"
-      //                 },
-      //                 {
-      //                     "caloriesOut": 0,
-      //                     "max": 220,
-      //                     "min": 160,
-      //                     "minutes": 120,
-      //                     "name": "Peak"
-      //                 }
-      //             ],
-      //             "restingHeartRate": 55
-      //         }
-      //     }
-      // ])
-      // }, 8000);
-
-      // setTimeout(function(){
-      //   setWeightData([
-      //     {"dateTime":"2011-04-27","value":175},
-      //     {"dateTime":"2011-04-28","value":174},
-      //     {"dateTime":"2011-04-29","value":173},
-      //     {"dateTime":"2011-04-30","value":175},
-      //     {"dateTime":"2011-05-01","value":180},
-      //     {"dateTime":"2011-05-02","value":174},
-      //     {"dateTime":"2011-05-03","value":173},
-      //   ]);
-      // }, 3000);
-
-      setTimeout(() => {
-        setWeight({
-          start: 160,
-          current: 143,
-          goal: 135,
-        })
-      }, 2000);
     }, []);
 
     return (
@@ -541,44 +269,39 @@ export default function DashboardContent() {
               </Paper>
             </Grid>
             {/* Exercise */}
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
               <Paper className={fixedHeightPaper}>
                 <Title>Exercise</Title>
               </Paper>
-            </Grid>
+            </Grid> */}
             {/* Heart Rate */}
             <Grid item xs={12} md={6} lg={6}>
               <Paper className={fixedHeightPaper}>
-                {/* <Title>Heart R/ate</Title> */}
                 <HeartRateZones heartRateData={heartRateData} />
               </Paper>
             </Grid>
             {/* Resting Heart Rate */}
             <Grid item xs={12} md={6} lg={6}>
               <Paper className={fixedHeightPaper}>
-                {/* <Title>Resting Heart Rate</Title> */}
                 <RestingHeartRate heartRateData={heartRateData} />
               </Paper>
             </Grid>
             {/* Weight */}
             <Grid item xs={12} md={6} lg={6}>
               <Paper className={fixedHeightPaper}>
-                {/* <Title>Weight</Title> */}
                 <Weight weightData={weightData} />
               </Paper>
             </Grid>
             {/* Weight goal */}
             <Grid item xs={12} md={3} lg={3}>
               <Paper className={fixedHeightPaper}>
-                <Title>Weight goal</Title>
-                {/* <WeightGoal start={weight.start} current={weight.current} goal={weight.goal} /> */}
+                <WeightGoal start={weight.start} current={weight.current} goal={weight.goal} />
               </Paper>
             </Grid>
             {/* BMI */}
             <Grid item xs={12} md={3} lg={3}>
               <Paper className={fixedHeightPaper}>
                 <Title>BMI</Title>
-                {/* <BMI /> */}
               </Paper>
             </Grid>
           </Grid>
---
+++ client/src/components/WeightGoal.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { PieChart, Pie, Label, ResponsiveContainer, Tooltip } from 'recharts';
+
+function CustomLabel({viewBox, value1, value2, value3}){
+    const {cx, cy} = viewBox;
+    return (
+     <text x={cx} y={cy} fill="#3d405c" className="recharts-text recharts-label" textAnchor="middle" dominantBaseline="central">
+        <tspan alignmentBaseline="middle" fontSize="22" x="50%">{`${value1} lb`}</tspan>
+        <tspan fontSize="24" x="50%" dy="1.25em">{`Start: ${value2} lb`}</tspan>
+        <tspan fontSize="24" x="50%" dy="1.25em">{`Goal: ${value3} lb`}</tspan>
+     </text>
+    )
+  }
+
+export default function WeightGoal(props) {
+    const remainingWeight = props.goal - props.current;
+
+    const [data, setData] = useState([
+        {name: "Weight", value: props.current, fill: '#8884d8'},
+        {name: "Remaining Weight", value: Math.abs(remainingWeight), fill: '#eee'},
+    ]);
+
+    useEffect(() => {
+        let current = props.current - props.start;
+        if ((props.start - props.goal) < 0){
+            current = current < 0 ? 0 : current;
+        } else {
+            current = current > 0 ? 0 : Math.abs(current);
+        }
+        setData([
+            {name: "Weight", value: current, fill: '#8884d8'},  
+            {name: "Remaining Weight", value: Math.abs(remainingWeight), fill: '#eee'},
+        ]);
+    }, [props.start, props.current, props.goal]);
+
+    return (
+    <React.Fragment>
+        <ResponsiveContainer>
+            <PieChart>
+                <Pie data={data} dataKey="value" nameKey="name"
+                    innerRadius='60%' outerRadius='90%'
+                    startAngle={180} endAngle={0}>
+                    <Label width={30} position="center"
+                    content={<CustomLabel value1={props.current} value2={props.start} value3={props.goal} />}>
+                        {`${props.current} lbs`}
+                    </Label>
+                </Pie>
+                <Tooltip position={{ x: 0, y: 0 }} />
+
+            </PieChart>
+        </ResponsiveContainer>
+    </React.Fragment>
+    );
+}
