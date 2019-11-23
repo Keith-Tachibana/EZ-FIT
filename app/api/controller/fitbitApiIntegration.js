407d9823ce19becec85a1e31bc3257ca2b8c7f42
Harry Pham <harrynp12@gmail.com>
2019-11-22 21:47:00 -0800

    Added back end for Fitbit api weight data integration

--- app/api/controller/fitbitApiIntegration.js
+++ app/api/controller/fitbitApiIntegration.js
@@ -174,4 +174,60 @@ async function getHeartRate(req, res, next) {
     }
 }
 
-module.exports = { getActivitySummary, getCaloriesBurned, getHeartRate };
+async function getWeightData(req, res, next) {
+    try {
+        const userInfo = await userModel.findById(req.body.userId);
+        const accessToken = userInfo.authToken.access_token;
+        const userId = userInfo.authToken.user_id;
+        const tokenStatus = await checkTokenStatus(userInfo);
+        if (tokenStatus === false) {
+            return res.json({
+                status: 'error',
+                message: "Couldn't refresh token",
+            });
+        }
+        try {
+            const resp = await axios.get(
+                `https://api.fitbit.com/1/user/${userId}/body/weight/date/today/30d.json`,
+                {
+                    headers: {
+                        Authorization: 'Bearer ' + accessToken,
+                        'Accept-Language': 'en_US',
+                    },
+                }
+            );
+            const weightData = resp.data;
+            res.json({
+                status: 'success',
+                message: 'Successfully retrieved weight data',
+                data:  weightData,
+            });
+        } catch (err) {
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+              } else if (err.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(err.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', err.message);
+              }
+              console.log(err.config);
+              res.json({
+                status: 'error',
+                message: 'Error getting weight data',
+                data: null,
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = { getActivitySummary, getCaloriesBurned, getHeartRate, getWeightData };
