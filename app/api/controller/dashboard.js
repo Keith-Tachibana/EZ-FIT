670a4e34052a877571a72a2843779cc9fb0ce9ab
Harry Pham <harrynp12@gmail.com>
2019-11-23 01:51:31 -0800

    Added better error handling

--- app/api/controller/dashboard.js
+++ app/api/controller/dashboard.js
@@ -108,8 +108,23 @@ async function getActivitySummary(req, res, next) {
                 data: resp.data,
             });
         } catch (err) {
-            console.log(err.response.data.errors);
-            res.json({
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+              } else if (err.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(err.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', err.message);
+              }
+              console.log(err.config);
+              res.json({
                 status: "error",
                 message: "Error retrieving activity summary",
                 data: null,
@@ -148,8 +163,23 @@ async function getCalories(req, res, next) {
                 data: {"activities-calories": caloriesBurnedData},
             });
         } catch (err) {
-            console.log(err.response.data.errors);
-            res.json({
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+              } else if (err.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(err.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', err.message);
+              }
+              console.log(err.config);
+              res.json({
                 status: "error",
                 message: "Error getting calories data",
                 data: null,
--- client/src/components/DashboardContent.js
+++ client/src/components/DashboardContent.js
@@ -146,7 +146,22 @@ export default function DashboardContent() {
           setCaloriesBurnedData(caloriesBurnedData['activities-calories']);
         }
       } catch (err) {
-        console.log(err.response);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(err.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', err.message);
+        }
+        console.log(err.config);
       }
     };
 
@@ -177,7 +192,22 @@ export default function DashboardContent() {
           });
         }
       } catch (err) {
-        console.log(err.response);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(err.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', err.message);
+        }
+        console.log(err.config);
       }
     };
 
@@ -189,7 +219,22 @@ export default function DashboardContent() {
           setHeartRateData(heartRateData['activities-heart']);
         }
       } catch (err) {
-        console.log(err.response);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(err.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', err.message);
+        }
+        console.log(err.config);
       }
     }
 
@@ -201,7 +246,22 @@ export default function DashboardContent() {
           setWeightData(weightData['body-weight']);
         }
       } catch (err) {
-        console.log(err.response);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(err.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', err.message);
+        }
+        console.log(err.config);
       }
     }
 
@@ -218,7 +278,22 @@ export default function DashboardContent() {
           });
         }
       } catch (err) {
-        console.log(err.response);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(err.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', err.message);
+        }
+        console.log(err.config);
       }
     }
 
