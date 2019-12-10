dafe0d1bdea0a8de26ee2039d4bc435869343225
Harry <harrynp12@gmail.com>
2019-12-10 00:14:35 -0800

    Changed ezfit icon to be more visible on tabs and homepage

--- client/src/components/EzfitIcon.js
+++ client/src/components/EzfitIcon.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function EzfitIcon() {
-    return <img src="favicon.ico" alt="EZ-Fit" />;
+    return <img src="icon-transparent-128x128.png" alt="EZ-Fit" />;
 }
